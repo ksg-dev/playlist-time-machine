395041116abd8cd1071f8c3bf416733e68bdb927
ksg-dev <ksg.dev.data@gmail.com>
2024-07-01 12:14:22 -0500

    auth and spotify search

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 from playlist import SpotAuth
 from billboard import BillboardData
 
-# time_travel_date = input("When would you like to go? Type date in YYYY-MM-DD: ")
-#
-# b_data = BillboardData(time_travel_date)
-# test_content = b_data.data_content
-# test_bb = b_data.make_soup()
-# test_songs = b_data.songs
-# test_art = b_data.artists
-#
-# print(f"test song: {test_songs}")
+time_travel_date = input("When would you like to go? Type date in YYYY-MM-DD: ")
+
+b_data = BillboardData(time_travel_date)
+test_content = b_data.data_content
+test_bb = b_data.make_soup()
+song_list = b_data.songs
+test_art = b_data.artists
+
+test_songs = ['Leave The Door Open', 'Peaches', 'Rapstar', 'alsidjhf']
+
+# print(f"test song: {song_list}")
 # print(f"test_art: {test_art}")
 
 auth_try = SpotAuth()
+song_uris = []
+
+# track = test_songs[0]
+# artist = test_art[0]
+# year = time_travel_date[:4]
+
+# Testing Error Handling
+for song in test_songs:
+    year = time_travel_date.split("-")[0]
+    song_search = auth_try.song_search(song, year)
+    song_uris.append(song_search)
+    print(song_search)
+
+# for song in song_list:
+#     year = time_travel_date.split("-")[0]
+#     song_search = auth_try.song_search(song, year)
+#     song_uris.append(song_search)
+#     print(song_search)
+
+print(song_uris)
+
+# song_search = auth_try.song_search(track, year)
+
+# print(track)
+# print(artist)
+# print(year)
+
--- playlist.py
+++ playlist.py
@@ -3,8 +3,10 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv()
+
 
 class SpotAuth:
     def __init__(self):
@@ -12,7 +14,8 @@ class SpotAuth:
         self._client_secret = os.environ["SPOTIFY_SECRET"]
         self.request_uri = "http://example.com"
         self.scope = "playlist-modify-private"
-        self.auth()
+        # self.session = self.auth()
+        self.current_user = self.auth().current_user()
 
     def auth(self):
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -21,4 +24,14 @@ class SpotAuth:
             redirect_uri=self.request_uri,
             scope=self.scope
         ))
-        curr_user = sp.current_user()
+        # curr_user = sp.current_user()
+        # print(sp.me())
+        return sp
+
+    def song_search(self, track, year):
+        query_str = f"track: {track} year: {year}"
+        result = self.auth().search(query_str, limit=1)
+        # pprint(result)
+        song_uri = result["tracks"]["items"][0]["uri"]
+        # pprint(song_uri)
+        return song_uri
